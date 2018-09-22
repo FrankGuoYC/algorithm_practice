00caf14ac63c134d6c107656fd8be3e95ed9b1ee
Frank Guo <yckuo5k2b@gmail.com>
2018-09-22 10:51:46 +0800

    Modify 8 queens puzzle

--- Eight queens puzzle/myanswer.c
+++ Eight queens puzzle/myanswer.c
@@ -3,28 +3,26 @@
 #define QUEENS 8 /*皇后数量、棋盤邊長*/
 #define IS_OUTPUT_TABLE 1 /*是否輸出具體解，1為輸出，0為不輸出*/
 
-// global的array的elements會全部初始化成0
-int up[QUEENS], upLeft[QUEENS * 2], upRight[QUEENS * 2], chess[QUEENS][QUEENS]; // 棋盤使用1-indexing
-/* k是棋盤大小，不過k[0][:](row 0)和k[:][0](col 0)貌似不會用到 */
-int inc; // 這邊看不懂，目前猜測是用來檢查Q是否有和其他Q在同一條線上
+// 棋盤使用0-indexing
+/**
+ * up用來記錄同一行是否已有皇后，upLeft紀錄左上-右下斜行是否已有皇后，upRight紀錄右上-左下斜行是否已有皇后，chess則為棋盤
+ * **/
+int up[QUEENS], upLeft[QUEENS * 2], upRight[QUEENS * 2], chess[QUEENS][QUEENS]; 
+int inc; // 用來記錄目前已經有幾個符合條件的棋盤
 
 // lay()代表的意義是?
 // 目前推測是放置皇后位置的意思，lay(x): 代表放置第x個皇后在棋盤第x列
 void lay(int i) {
-  int j = 0, t, u;  // t,u只是為了print出table的2層for迴圈的counter
+  int j = 0, t, u;  // temp var for the nested for-loop
 
   while (j < QUEENS) {
-    // a[j]代表? b[j-i]代表? c[j+i]代表?
-    // a[j]不會超出陣列範圍嗎?
-    // 陣列空間是連續配置的，所以，初步假設a有可能會access到B，b有B可能會access到A、C，c有可能會access到B?
     if (up[j] + upLeft[j - i + QUEENS] + upRight[j + i] == 0) {
       chess[i][j] = up[j] = upLeft[j - i + QUEENS] = upRight[j + i] = 1; // 設成1，代表放置皇后/該排有皇后
-      // a[j], b[j - i], c[j + i]設成1的意義是?
       if (i < QUEENS-1) lay(i + 1);   // 進行下一個皇后的放置
-      else {    // i等於QUEENS的時候才會進來這個else
+      else {    // i等於QUEENS-1的時候才會進來這個else
         ++inc;  // 用來記錄是第幾個八皇后棋盤
         if (IS_OUTPUT_TABLE) {
-            // print出一個table
+          // print出符合條件的table
           for (printf("(%d)\n", inc), u = 0; u < QUEENS; u++, printf("\n"))
             for (t = 0; t < QUEENS; t++) chess[t][u] ? printf("Q ") : printf("+ ");
           printf("\n\n");
@@ -38,7 +36,7 @@ void lay(int i) {
 
 int main(void) {
   lay(0);
-  printf("%d queens: %d solutions", QUEENS, inc);
+  printf("%d queens: %d solutions\n", QUEENS, inc);
 //   getchar();
   return 0;
 }
